fc072c708798f519ddb06c8949f12cecd81101de
josiahschaefer <penname.silas@gmail.com>
2015-09-19 20:15:30 +0000

    added tests for video and category

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,10 +1,7 @@
 require 'spec_helper'
 
 describe Video do
-  it "saves itself" do
-    video = Video.new(title: "Pokemon", description: "cute pocket monsters", small_cover: "http://ocala.cgashows.com/wp-content/uploads/2015/03/Pokemon.jpg",
-      large_cover: "http://ocala.cgashows.com/wp-content/uploads/2015/03/Pokemon.jpg")
-    video.save
-    Video.find(2).title.should == "Pokemon"
-  end
+  it { should belong_to(:category)}
+  it { should validate_presence_of(:title)}
+  it { should validate_presence_of(:description)}
 end
