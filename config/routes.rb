bc34c05a3d4958b509d96de26750c105dff3864c
josiahschaefer <penname.silas@gmail.com>
2015-09-19 15:51:56 +0000

    Setting up project, home page, show pages, files

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Myflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
+  
+  root 'categories#index'
+  
+  resources :videos, only: [:index, :show]
+  
+  resources :categories, only: [:index, :show]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Video.create(title: "Sword Art Online", description: "Stuck in a virtual world",
+  small_cover: "http://images6.fanpop.com/image/photos/34900000/sword-art-online-sword-art-online-fans-34959044-1280-1024.jpg",
+  large_cover: "http://images6.fanpop.com/image/photos/34900000/sword-art-online-sword-art-online-fans-34959044-1280-1024.jpg")
+
+Category.create(category_name: "Anime")
